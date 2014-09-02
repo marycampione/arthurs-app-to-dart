646052c00386a41f76fc656658ef14fa5ee09638
Mary Campione <mcampione@google.com>
2014-09-02 08:22:03 -0700

    working intermediate step

--- web/post-card.dart
+++ web/post-card.dart
@@ -2,13 +2,20 @@
 
 @CustomTag('post-card')
 class PostCard extends PolymerElement {
-  var text;
-  var avatar;
-  var username;
+  
+  //@observable bool favorite;
   
   PostCard.created() : super.created() {
-    print(text);
-    print(avatar);
-    print(username);
+  }
+  
+  favoriteTapped(event, detail, sender) {
+    //favorite = !favorite;
+    //print(favorite);
+    
+    // Where is the boolean favorite? How is this class associated with post in myPosts
+    // need to invert value of favorite. Is it in sender?
+    print(detail.toString());
+    
+    this.fire('favorite-tap');
   }
 }
--- web/post-list.dart
+++ web/post-list.dart
@@ -1,10 +1,25 @@
 import 'package:polymer/polymer.dart';
+import 'dart:html';
 
 @CustomTag('post-list')
 class PostList extends PolymerElement {
   
   @observable List<List> myPosts = toObservable([]);
 
+  @observable String show;
+  
   PostList.created() : super.created() {
+    var list = document.querySelector('post-list');
+    var tabs = document.querySelector('paper-tabs');
+
+    tabs.addEventListener('core-select', (e) {
+      list.show = tabs.selected;
+    });
+  }
+  
+
+  handleFavorite(event, detail, sender) {
+    var post = sender.templateInstance.model.post;
+    //this.$.service.setFavorite(post.uid, post.favorite);
   }
 }
